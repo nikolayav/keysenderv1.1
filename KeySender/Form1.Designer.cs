fbd51dbdccead0c93ec87aec9675f92ac85c1003
nikolayav <nikolay.avroniev@dxc.com>
2020-04-09 15:55:14 +0300

    Add project files.

---
+++ KeySender/Form1.Designer.cs
@@ -0,0 +1,108 @@
+﻿namespace KeySender {
+    partial class Form1 {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing) {
+            if (disposing && (components != null)) {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent() {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.tmrSendKeys = new System.Windows.Forms.Timer(this.components);
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.helpToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.AboutToolstripButton = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.richTextBox1.Location = new System.Drawing.Point(0, 24);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(507, 302);
+            this.richTextBox1.TabIndex = 1;
+            this.richTextBox1.Text = "";
+            // 
+            // tmrSendKeys
+            // 
+            this.tmrSendKeys.Tick += new System.EventHandler(this.tmrSendKeys_Tick);
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.helpToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(507, 24);
+            this.menuStrip1.TabIndex = 5;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // helpToolStripMenuItem
+            // 
+            this.helpToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.AboutToolstripButton});
+            this.helpToolStripMenuItem.ImageAlign = System.Drawing.ContentAlignment.TopCenter;
+            this.helpToolStripMenuItem.Name = "helpToolStripMenuItem";
+            this.helpToolStripMenuItem.Size = new System.Drawing.Size(44, 20);
+            this.helpToolStripMenuItem.Text = "Help";
+            // 
+            // AboutToolstripButton
+            // 
+            this.AboutToolstripButton.ImageAlign = System.Drawing.ContentAlignment.TopCenter;
+            this.AboutToolstripButton.Name = "AboutToolstripButton";
+            this.AboutToolstripButton.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.AboutToolstripButton.RightToLeftAutoMirrorImage = true;
+            this.AboutToolstripButton.Size = new System.Drawing.Size(180, 22);
+            this.AboutToolstripButton.Text = "About";
+            this.AboutToolstripButton.TextDirection = System.Windows.Forms.ToolStripTextDirection.Horizontal;
+            this.AboutToolstripButton.Click += new System.EventHandler(this.AboutToolstripButton_Click);
+            // 
+            // Form1
+            // 
+            this.ClientSize = new System.Drawing.Size(507, 326);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.menuStrip1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MainMenuStrip = this.menuStrip1;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Key Sender";
+            this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.Form1_FormClosed);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Timer tmrSendKeys;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem helpToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem AboutToolstripButton;
+    }
+}
+
---
+++ KeySender/Form1.cs
@@ -0,0 +1,78 @@
+﻿using SnagFree.TrayApp.Core;
+using System;
+using System.Text;
+using System.Windows.Forms;
+using System.Windows.Input;
+
+namespace KeySender {
+    public partial class Form1 : Form {
+        public Form1() {
+            InitializeComponent();
+        }
+        private GlobalKeyboardHook _globalKeyboardHook;
+        private void Form1_Load(object sender, EventArgs e) {
+            _globalKeyboardHook = new GlobalKeyboardHook();
+            _globalKeyboardHook.KeyboardPressed += OnKeyPressed;
+        }
+
+        private void OnKeyPressed(object sender, GlobalKeyboardHookEventArgs e) {
+            if (e.KeyboardState == GlobalKeyboardHook.KeyboardState.KeyUp) {
+                //detect a right-ctrl key-up
+                if ((e.KeyboardData.HardwareScanCode == 29) && (e.KeyboardData.VirtualCode == 163)) {
+                    IntPtr windowdHandle = NativeWin32.GetForegroundWindow();
+                    //If the foreground window is this application (Key Sender) then ignore it.
+                    if (!windowdHandle.Equals(this.Handle)) {
+                        tmrSendKeys.Start();
+                    }
+                }
+            }
+        }
+
+        private void Form1_FormClosed(object sender, FormClosedEventArgs e) {
+         
+        }
+
+        private void SendText() {
+            char[] specialChars = { '{', '}', '(', ')', '+', '^' };
+            string str = richTextBox1.Text;
+
+            foreach (char letter in str) {
+                bool isSpecialChar = false;
+
+                for (int i = 0; i < specialChars.Length; i++) {
+                    if (letter == specialChars[i]) {
+                        isSpecialChar = true;
+                        break;
+                    }
+                }
+
+                if (isSpecialChar)
+                    SendKeys.Send("{" + letter.ToString() + "}");
+                else
+                    SendKeys.Send(letter.ToString());
+            }
+        }
+
+        private void tmrSendKeys_Tick(object sender, EventArgs e) {
+            tmrSendKeys.Stop();
+            SendText();
+        }
+
+        private void AboutToolstripButton_Click(object sender, EventArgs e) {
+            
+            string usageInfo = "Name: Key Sender\n" +
+                               "Version: 1.0\n" +
+                               "Author: Nikolay Avroniev\n\n" +
+                               "Description: A simple application that sends individual keystrokes to an external textfield.\n\n" +
+                               
+                               "Usage:\n" +
+                               "1. Type the text you need copied in the textbox field. \n" +
+                               "2. Select a destination text field. \n" +
+                               "3. Press \"Right-Ctrl\" button.";
+           
+            MessageBox.Show(usageInfo, "About", MessageBoxButtons.OK, MessageBoxIcon.None);
+            
+        }
+    }
+}
+
